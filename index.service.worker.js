f4e91c100107af4a01a34e63ceb915e05341bd06
SteadyStateGames <steadystategames@gmail.com>
2026-09-17 22:04:32 -0600

    Publish Barrage Blockade web app

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789378352|2904310';
+const CACHE_VERSION = '1789704093|3040175';
 /** @type {string} */
 const CACHE_PREFIX = 'Barrage Blockade-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
